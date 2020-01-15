b59bef854923343ea5e775264201c5a3055f1c7f
selim <sftekin25@gmail.com>
2020-01-15 15:18:37 +0300

    experiments

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ model_config = {
     'LinSVM': {
         'c_list': [1],
         'tol': [1e-4],
-        'cv': 1,
+        'cv': 3,
         'scoring': 'f1_micro',
         'model_name': 'linear_svm',
         'load': False
@@ -10,7 +10,7 @@ model_config = {
     'RandomForest': {
         'n_estimator': [3000],
         'max_depth': [1000],
-        'cv': 1,
+        'cv': 3,
         'scoring': 'f1_micro',
         'model_name': 'random_forest',
         'load': False
